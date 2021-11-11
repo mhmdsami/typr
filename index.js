2c3e672a4f028eb978d8f9f7c1e4c9d3e5e3ba9c
sm-sami <srmf2025@gmail.com>
2021-11-11 23:21:44 +0530

    WPM for words

--- index.js
+++ index.js
@@ -7,18 +7,50 @@ const quoteStartBtn = document.querySelector(".quote-start-btn");
 const wordsAPI = count => (`https://random-word-api.herokuapp.com/word?number=${count}`);
 const quoteAPI = 'https://api.quotable.io/random';
 
+let interval = 0;
+
+const startTimer = () => {
+    ++interval;
+}
 
 const getWords = async (count) => {
     const res = await fetch(wordsAPI(count));
     const words = await res.json();
-    wordsContainer.innerText = ''
-    words.forEach( (word, index) => {
+    wordsContainer.innerText = ""
+    let wordLength = 0;
+    words.forEach((word, index) => {
         const wordSpan = document.createElement("span");
         wordSpan.innerText = word;
         if(index == 0){
             wordSpan.setAttribute("class", "focused")
         }
         wordsContainer.appendChild(wordSpan);
+        wordLength += word.length;
+    });
+
+    setInterval(startTimer, 1000);
+    wordInput.addEventListener("input", () => {
+        words.forEach((word, index) => {
+            if(word == wordInput.value && word != words[words.length - 1]){
+                focusedSpan = document.querySelector(".focused");
+                focusedSpan.nextSibling.setAttribute("class", "focused");
+                focusedSpan.removeAttribute("class", "focused");
+                wordInput.addEventListener("keyup", event => {
+                    if(event.code == "Space" && word != words[words.length - 1]){
+                        wordInput.value = '';
+                    }
+                });
+            }else if(word == wordInput.value && word == words[words.length - 1]){
+                document.querySelector(".focused").removeAttribute("class", "focused");
+                wordInput.addEventListener("keyup", event => {
+                    if(event.code == "Space" || event.code == "Enter"){
+                        wordInput.value = '';
+                        clearInterval(startTimer);
+                    wordsContainer.innerText = `${wordLength * 60 / interval} WPM`;
+                    }
+                });
+            }
+        });
     });
 }
 
